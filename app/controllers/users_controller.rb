addab2bf2fa20ccc961c7876f8cea078f67c2af5
杉山佑里子 <peaceful.future1101@gmail.com>
2016-09-18 00:10:47 +0900

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if@user.save
+      redirect_to users_path , notice:'ユーザ登録が完了しました'
+    else
+      render :new
+      
+    end
+  end
+  
+
+  def destroy
+   @user.destroy
+   redirect_to users_path , notice:'ユーザを削除しました'
+  end
+
+  def edit
+    
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to users_path , notice:'ユーザを更新しました'
+    else
+      render :edit
+      
+    end
+  end
+  
+      def show
+  
+      end
+
+      private
+      def user_params
+        #   フォームに不正な値が入ってないかフィルタリング
+        params.require(:user).permit(:email,:password)
+      end
+  
+      def set_user
+        @user = User.find(params[:id])
+      end
+    end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+ 
+
+
+  devise_for :users
+
+   devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
+  # namespace :admin do
+resources :users
+ 
+#  end
+  
+   
+    
+  root 'pages#top'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
